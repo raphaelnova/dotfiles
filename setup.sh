f56d3cb0c5c317aedcc67bff2cb548b4ef768248
Raphael Nova <rafa.boanova@gmail.com>
2014-11-30 23:10:08 -0200

    Fixing setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find ./home/ -maxdepth 1 -print0 | while read -rd $'\0' FILE; do
+find ./home/ -maxdepth 1 \! -path "./home/" -print0 | while read -rd $'\0' FILE; do
   FILENAME="$(basename $FILE)"
-  ln -fs ~/dotfiles/home/ ~/
+  ln -fs ~/dotfiles/home/$FILENAME ~/
 done
 
