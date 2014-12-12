cb9f2f5154d1fbee67e99f478b5defc909d57aaf
Raphael Nova <rafa.boanova@gmail.com>
2014-12-11 23:41:32 -0200

    Solarized pantheon-term palette and dircolors

---
+++ scripts/solarizedDark_pantheon-term_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Solarized dark pantheon-terminal settings
+gsettings set org.pantheon.terminal.settings cursor-color '#93a1a1'
+gsettings set org.pantheon.terminal.settings background '#002b36'
+gsettings set org.pantheon.terminal.settings foreground '#839496'
+gsettings set org.pantheon.terminal.settings opacity 100
+gsettings set org.pantheon.terminal.settings palette '#073642:#dc322f:#859900:#b58900:#268bd2:#d33682:#2aa198:#eee8d5:#002b36:#cb4b16:#586e75:#657b83:#839496:#6c71c4:#93a1a1:#fdf6e3'
--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ cd "$(dirname "$0")"
 FOLDER="$(pwd)/home/"
 find "$FOLDER" -maxdepth 1 \! -path "$FOLDER" -exec ln -fs "{}" ~/ \;
 
+# Setup solarized on pantheon-terminal
+. scripts/solarizedDark_pantheon-term_setup.sh
