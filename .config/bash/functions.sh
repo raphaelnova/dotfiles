12eec742844813e1a1cb406a922b207517cb1b04
Raphael Nova <contact@raphaelnova.info>
2025-10-17 00:53:13 -0300

    Small improvements in PS1 functions + documentation
    `__marker_path` now takes a list of arguments and returns on the
    first one found. `__bg_jobs` ignores detached processes and only
    counts suspended ones. Comments all over the place.

--- .config/bash/functions.sh
+++ .config/bash/functions.sh
@@ -1,37 +1,58 @@
+# Given a list of files, traverses the filesystem upwards
+# searching for the first it can find. Useful to identify
+# whether the cwd is inside a project folder (by finding
+# e.g. a pom.xml or a package.json). Prints the full file
+# path.
 __marker_path () {
-    local marker="$1"
     local dir="$PWD"
+    local markers=("$@")
+    local marker
     while [[ "$dir" != "/" ]]; do
-        if [[ -f "$dir/$marker" ]]; then
-            printf "%s/%s" "$dir" "$marker"
-            return
-        fi
+        for marker in "${markers[@]}"; do
+            if [[ -f "$dir/$marker" ]]; then
+                printf "%s/%s" "$dir" "$marker"
+                return
+            fi
+        done
         dir="$(dirname "$dir")"
     done
 }
 
+# Prints a count of suspended jobs. Used in PS1 so I don't
+# lose track of a suspended nvim and `exec bash` losing it
+# in the void.
 __bg_jobs () {
     local job_count
-    job_count="$(jobs | wc -l)"
+    # Count only suspended processes (not ending in &)
+    job_count="$(jobs | grep -cvE '&$')"
     if [[ "$job_count" -gt 0 ]]; then
         printf " [%s]" "$job_count"
     fi
 }
 
+# Prints a short identifier to indicate the current Java
+# version active. Used in PS1.
 __javaversion () {
     if command -v java >/dev/null 2>&1 && [ -n "$(__marker_path pom.xml)" ]; then
         local version
         version="$(java -version 2>&1)"
         case "$version" in
+            # Azul JDK
             *Zulu21*)     local JAVA_VERSION="z21" ;;
             *Zulu17*)     local JAVA_VERSION="z17" ;;
             *Zulu11*)     local JAVA_VERSION="z11" ;;
+
+            # Eclipse Temurin
             *Temurin-23*) local JAVA_VERSION="t23" ;;
             *Temurin-25*) local JAVA_VERSION="t25" ;;
+
+            # OpenJDK
             *build\ 21*)  local JAVA_VERSION="j21" ;;
             *build\ 17*)  local JAVA_VERSION="j17" ;;
             *build\ 11*)  local JAVA_VERSION="j11" ;;
             *build\ 1.8*) local JAVA_VERSION="j8"  ;;
+
+            # What?
             *)            local JAVA_VERSION="??"  ;;
         esac
 
@@ -42,6 +63,8 @@ __javaversion () {
     fi
 }
 
+# Are we inside a Maven project folder? Prints the artifactId.
+# Used in PS1.
 __mvn_proj () {
     local xpath="/*[name()='project']/*[name()='artifactId']/text()"
     local pom
@@ -56,6 +79,8 @@ __mvn_proj () {
     fi
 }
 
+# Are we inside a python venv? Prints the root folder name.
+# Used in PS1.
 __pyvenv () {
     if [ -n "$VIRTUAL_ENV" ]; then
         local ENV_NAME
@@ -67,6 +92,8 @@ __pyvenv () {
     fi
 }
 
+# Are we inside a node project folder? Print the node version.
+# Used in PS1
 __nodejsversion () {
     if command -v node >/dev/null 2>&1 && [ -n "$(__marker_path package.json)" ]; then
         local VERSION
@@ -82,7 +109,10 @@ __nodejsversion () {
     fi
 }
 
+# Archetipe to generate a new Maven project. Because
+# Maven archetype:generate commands are too verbose.
 mvn_new_proj () {
+    # TODO: maybe parse short options here?
     local group_id="$1"
     local artifact_id="$2"
     local package=""
@@ -104,11 +134,13 @@ mvn_new_proj () {
       -DinteractiveMode="$interactive"
 }
 
+# A fresh UUID sent to stdout and to xclip
 uuid () {
-    uuidgen | tr -d '\n' | tee >(xclip -sel clip)
-    echo
+    uuidgen | tee >(tr -d '\n' | xclip -sel clip)
 }
 
+# Given optional size and charset, generates a random string.
+# Prints a string of 8 digits by default.
 randStr () {
     local size="${1:-8}"
     local chars="${2:-0123456789}"
@@ -119,6 +151,7 @@ randStr () {
     echo
 }
 
+# Not my code. Prints an 8bit color pallete.
 colors () {
     local T='gYw'
 
@@ -142,6 +175,7 @@ colors () {
     echo
 }
 
+# Not mine either. Prints a 16bit color pallete.
 colors_256 () {
     # 0-7: Standard colors
     # 8-15: High-intensity colors
@@ -159,6 +193,9 @@ colors_256 () {
     echo -e "\e[0m"
 }
 
+# Gets the current java-alternative and sets its path to JAVA_HOME.
+# Not necessary anymore now that I'm using SDKMAN, but I don't feel
+# like throwing it away yet.
 update_java_home () {
     local CURR_JAVAC_BIN
     CURR_JAVAC_BIN="$(update-alternatives --get-selections | awk '/^javac/{ print $3 }')"
@@ -166,6 +203,9 @@ update_java_home () {
     echo "JAVA_HOME is now \"$JAVA_HOME\""
 }
 
+# Given a short identifier, sets a Java version from a set of
+# java-alternatives. Not in use it anymore since I'm using
+# SDKMAN.
 set_java () {
     local JAVA_NAME
     case "$1" in
@@ -186,6 +226,10 @@ set_java () {
     update_java_home
 }
 
+# Gets git commit hashes from the history and shows each
+# commit in chronological order. Make sure not to use it
+# in large projects because this is not limiting how many
+# commits to show.
 traverse_git_hist() {
     git log --pretty=format:"%H" \
         | cat - <(echo) \
@@ -195,6 +239,7 @@ traverse_git_hist() {
           done
 }
 
+# Given a video file, outputs a 480w gif from it.
 to_gif () {
     if [ "$#" -ne 2 ]; then
         echo "Usage: $0 input-video output-gif"
