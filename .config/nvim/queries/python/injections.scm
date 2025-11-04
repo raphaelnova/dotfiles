3d11ed25cb61a766f4b15007b5e031b92e3daae5
Raphael Nova <contact@raphaelnova.info>
2025-11-04 01:53:14 -0300

    [meh] Adds `; extends` to Bash injections, shrugs on Python SQL injections

--- .config/nvim/queries/python/injections.scm
+++ .config/nvim/queries/python/injections.scm
@@ -1,5 +1,6 @@
 ; extends
 
+;; It's crude but it works
 ((string_content) @injection.content
     (#match? @injection.content "(CREATE|DROP|SELECT|INSERT|UPDATE|DELETE)")
     (#set! injection.language "sql"))
