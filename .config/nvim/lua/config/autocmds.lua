9142ab54783645b62f8b40de8dee8c836392c5eb
Raphael Nova <contact@raphaelnova.info>
2025-11-30 18:58:31 -0300

    Descriptions for autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,16 +2,18 @@ local buffer_group = vim.api.nvim_create_augroup("buf_cmds", { clear = true })
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	pattern = "*",
+	group = buffer_group,
+	desc = "Disables native highlight color (I prefer using a plugin).",
 	callback = function()
 		vim.lsp.document_color.enable(false)
 		-- vim.lsp.document_color.enable(true, 0, { style = 'virtual' })
 	end
 })
 
--- Config module hot-reload
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "lua/config/*.lua",
 	group = buffer_group,
+	desc = "Hot-reloads any changed Lua module.",
 	callback = function(args)
 		local module_name = (vim.loop.cwd() .. "/" .. args.file)
 			 :gsub("^" .. vim.fn.stdpath("config") .. "/lua/", "")
@@ -20,36 +22,41 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 			 :gsub("%.init$", "")
 		package.loaded[module_name] = nil
 		require(module_name)
-		-- vim.notify("Reloaded " .. module_name, vim.log.levels.INFO)
 	end,
 })
 
--- disable relative line numbers on insert mode
 vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
 	pattern = "*",
 	group = buffer_group,
+	desc = "Disables relative line numbers when in Insert mode.",
 	callback = function(autocmd)
 		vim.opt.relativenumber = (autocmd.event == "InsertLeave")
 	end,
 })
 
 --
--- Highlight trailing spaces only out of insert mode
+-- Dynamic highlight for trailing spaces (only when out of insert mode).
 --
 vim.api.nvim_create_autocmd("BufWinEnter", {
-	-- Match trailing spaces and highlight them
+	pattern = "*",
+	group = buffer_group,
+	desc = "Matches trailing spaces and creates a highlight group for them.",
 	callback = function()
 		vim.fn.matchadd("TrailingWhitespace", "\\s\\+$")
 	end,
 })
 vim.api.nvim_create_autocmd("InsertEnter", {
-	-- Disable trailing spaces highilighting when in Insert mode
+	pattern = "*",
+	group = buffer_group,
+	desc = "Disables trailing space highlighting when in Insert mode.",
 	callback = function()
 		vim.api.nvim_set_hl(0, "TrailingWhitespace", { link = "Whitespace" })
 	end,
 })
 vim.api.nvim_create_autocmd("InsertLeave", {
-	-- Enable trailing spaces highilighting when out of Insert mode
+	pattern = "*",
+	group = buffer_group,
+	desc = "Enables trailing spaces highlighting when out of Insert mode.",
 	callback = function()
 		vim.api.nvim_set_hl(0, "TrailingWhitespace", { link = "TrailingWhitespaceStyle" })
 	end,
