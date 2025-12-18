c9b92dfa7cc0d7d327f238a09a6423342410d4ea
Raphael Nova <contact@raphaelnova.info>
2025-12-18 01:00:55 -0300

    Migrates nvim-treesitter to main (finally!), updates lazy plugins
    Took me a while to make it work. Had to install Rust toolchain and
    cargo install tree-sitter-cli because the binary that comes from
    npm is incompatible with Ubuntu's glibc.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,63 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
+	branch = "main",
+	lazy = false,
+	-- dependencies = {
+	-- 	"windwp/nvim-ts-autotag",
+	-- },
 	build = ":TSUpdate",
 	config = function()
-		---@diagnostic disable [missing-fields]
-		require("nvim-treesitter.configs").setup({
-			auto_install = true,
-			ensure_installed = {
-				"awk",
-				"bash",
-				"css",
-				"gitignore",
-				"haskell",
-				"html",
-				"java",
-				"javascript",
-				"json",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"typescript",
-				"sql",
-				"vim",
-				"vimdoc",
-				"xml",
-				"yaml",
-			},
-			highlight = { enable = true },
-			autotag = { enable = true },
-			indent = { enable = true },
-			fold = { enable = true },
+		local treesitter = require("nvim-treesitter")
+
+		treesitter.install({
+			"awk",
+			"bash",
+			"css",
+			"gitignore",
+			"haskell",
+			"html",
+			"javascript",
+			"json",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"typescript",
+			"sql",
+			"vim",
+			"vimdoc",
+			"yaml",
+		})
+
+		local ignore_filetypes = {
+			"checkhealth",
+			"lazy",
+			"mason",
+			"NvimTree",
+		}
+
+		local augroup = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
+		vim.api.nvim_create_autocmd("FileType", {
+			group = augroup,
+			desc = "Enable treesitter.",
+			callback = function(ev)
+				if vim.tbl_contains(ignore_filetypes, ev.match) then
+					return
+				end
+
+				local buf = ev.buf
+				local lang = vim.treesitter.language.get_lang(ev.match) or ev.match
+
+				treesitter.install({ lang }):await(function(err)
+					if err then
+						vim.notify("Treesitter failed to install parser for " .. lang .. ". Error: " .. err)
+						return
+					end
+					pcall(vim.treesitter.start, buf, lang)
+					vim.opt.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.opt.foldmethod = "expr"
+				end)
+			end,
 		})
 	end,
 }
