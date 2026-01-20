098113178a0e6e8b4d490c685dcd8c25a192d6b9
Raphael Nova <contact@raphaelnova.info>
2026-01-20 20:49:07 -0300

    nvim-tree floating window

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -3,6 +3,20 @@ return {
 	event = "VimEnter",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local function open_win_config_func()
+			local scr_w = vim.opt.columns:get()
+			local scr_h = vim.opt.lines:get()
+			local tree_w = 120
+			local tree_h = math.floor(tree_w * scr_h / scr_w)
+			return {
+				border = "single",
+				relative = "editor",
+				width = tree_w,
+				height = tree_h,
+				col = (scr_w - tree_w) / 2,
+				row = (scr_h - tree_h) / 2,
+			}
+		end
 		-- :h nvim-tree-quickstart-help
 		require("nvim-tree").setup({
 			hijack_netrw = true,
@@ -11,7 +25,10 @@ return {
 				enable = false,
 			},
 			view = {
-				width = {}, -- dynamic based on longest line
+				float = {
+					enable = true,
+					open_win_config = open_win_config_func,
+				},
 			},
 			renderer = {
 				group_empty = true,
