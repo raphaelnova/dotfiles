4a5ea0f80ce1884788c927835197e9de143cded6
Raphael Nova <contact@raphaelnova.info>
2026-01-25 19:22:08 -0300

    Fixes and simplifies Lualine config (minimalist looks)

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,50 +7,66 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "catppuccin",
-				section_separators = { -- remove to default to   (\ue0b0, \ue0b2)
-					left = "",
-					right = "",
+				theme = {
+					inactive = { a = { bg = "#161616", fg = "#666666" } },
+					normal = {
+						a = { bg = "#161616", fg = "#eeeeee" },
+						b = { bg = "#161616", fg = "#eeeeee" },
+						c = { bg = "#161616", fg = "#eeeeee" },
+					},
+					-- insert, command, terminal, visual, replace
 				},
-				component_separators = { -- remove to default to   (\ue0b1, \ue0b3)
-					left = ":",
-					right = ":",
-				},
-				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
-				},
+				section_separators = { left = "", right = "", },
+				component_separators = { left = ":", right = ":", },
+				disabled_filetypes = { statusline = {}, winbar = {}, },
 				ignore_focus = { "NvimTree" },
 				always_divide_middle = true,
-				global_status = false,
+				globalstatus = false,
 				refresh = { -- in ms
 					statusline = 1000,
 					tabline = 1000,
 					winbar = 1000,
 				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = { "branch", "diff", "diagnostics" },
-					lualine_c = { "filename" },
-
-					lualine_x = { "encoding", "fileformat", "filetype" },
-					lualine_y = { "progress" },
-					lualine_z = { "location" },
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = { "filename" },
-
-					lualine_x = { "location" },
-					lualine_y = {},
-					lualine_z = {},
-				},
-				tabline = {},
-				winbar = {},
-				inactive_winbar = {},
-				extensions = {},
 			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {},
+				lualine_c = { "filename" },
+
+				lualine_x = {
+					{
+						"diagnostics",
+						-- colored = false,
+						symbols = {
+							error = "✖ ",
+							warn = "▲ ",
+							info = "⚑ ",
+							hint = "✚ ",
+						},
+						-- diagnostics_color = {
+						-- 	error = "DiagnosticError",
+						-- 	warn = "DiagnosticWarn",
+						-- 	info = "DiagnosticInfo",
+						-- 	hint = "DiagnosticHint",
+						-- },
+					},
+				},
+				lualine_y = {},
+				lualine_z = {},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = {},
 		})
 	end,
 }
